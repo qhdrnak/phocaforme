496563a962760aaff4aabb0e4ccfe7b49752fe3e
MinY-Lee <leemy08@naver.com>
2024-01-23 01:22:23 +0900

    [INIT] BackEnd 초기 설정 완료

---
+++ BackEnd/src/main/java/com/phofor/phocaforme/PhocaFormeApplication.java
@@ -0,0 +1,13 @@
+package com.phofor.phocaforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhocaFormeApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(PhocaFormeApplication.class, args);
+   }
+
+}
---
+++ BackEnd/src/test/java/com/phofor/phocaforme/PhocaFormeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phofor.phocaforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhocaFormeApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
