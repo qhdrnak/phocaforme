2ae1f0481a316e1f23bcdfb72fb6979ee78a6a76
Jungilgyu <wjddlfrb131@naver.com>
2024-01-31 17:49:01 +0900

    feat: Add login

---
+++ FrontEnd/src/components/Login/OAuth.jsx
@@ -0,0 +1,5 @@
+
+const Client_id = '0c667ea3c8f35cc83690ec0c37f136e2'
+const Redirect_URI = 'https://example.com/oauth'
+
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${Client_id}&redirect_uri=${Redirect_URI}&response_type=code`
