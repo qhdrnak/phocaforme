b4dab4a32a6935e591fc3bf691e2982ea7cee21e
Jungilgyu <wjddlfrb131@naver.com>
2024-02-02 16:35:29 +0900

    fix: Fix error2

--- FrontEnd/src/components/NavBar/NonLoginImage.jsx
+++ FrontEnd/src/components/NavBar/NonLoginImage.jsx
@@ -76,30 +76,6 @@ const NonLoginIcon = () => {
             theme={theme}
           >
             <Divider variant="middle" component="li" />
-<<<<<<< Updated upstream
-            <Typography
-              sx={{
-                textAlign: "center",
-                color: theme.palette.primary.main,
-                p: 2,
-                fontSize: "1.2rem", // 글자 크기 조절
-                fontWeight: "bold", // 굵은 글자
-              }}
-            >
-              로그인이 필요한
-              <br />
-              서비스입니다.
-            </Typography>
-            <img
-              id="kakao-button"
-              src={kakao}
-              onClick={GotoLogin}
-              style={{
-                display: "block", // 이미지를 블록 요소로 설정하여 위아래 여백 추가
-                margin: theme.spacing(2), // 상단 여백 추가
-              }}
-            />
-=======
 						<Typography
 							sx={{ 
 								textAlign: 'center', 
@@ -121,7 +97,6 @@ const NonLoginIcon = () => {
 								margin: theme.spacing(2), // 상단 여백 추가
 							}}
 							/>
->>>>>>> Stashed changes
           </Menu>
         </Paper>
       </Box>
