79caa04bc79761b4b333c4ca55e034f29db83453
MinY-Lee <leemy08@naver.com>
2024-02-05 22:06:29 +0900

    add: 채팅방의 마지막 채팅 전송

--- BackEnd/src/main/java/com/phofor/phocaforme/chat/dto/response/ChatRoomResponseDto.java
+++ BackEnd/src/main/java/com/phofor/phocaforme/chat/dto/response/ChatRoomResponseDto.java
@@ -1,5 +1,6 @@
 package com.phofor.phocaforme.chat.dto.response;
 
+import com.phofor.phocaforme.chat.entity.ChatMessage;
 import com.phofor.phocaforme.chat.entity.ChatRoom;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,11 +12,13 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 public class ChatRoomResponseDto {  // 여기는 이미 디비에 반영이 된 시점
+
     private Long chatRoomId; //
     private Long boardId;    // 해당 교환 게시글id
     private String ownerId; // 게시글 작성자
     private String visiterId;   // 교환 희망자
     private String boardTitle;  // 게시글 제목
+    private ChatMessage latestChat;  // 가장 마지막 채팅
     private LocalDateTime createdAt;    // 채팅방 생성 날짜
 
 //    public ChatRoomResponseDto(ChatRoomRequestDto chatRoomRequestDto) {
@@ -34,6 +37,7 @@ public class ChatRoomResponseDto {  // 여기는 이미 디비에 반영이 된 
                 .boardTitle(chatroom.getBoardTitle())
                 .ownerId(chatroom.getOwnerId())
                 .visiterId(chatroom.getVisiterId())
+                .latestChat(chatroom.getChatLatest())
                 .createdAt(chatroom.getCreatedAt())
                 .build();
     }
