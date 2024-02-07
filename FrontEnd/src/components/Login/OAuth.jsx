77de020347baf642c8b6941a48d71ca7a724ac58
Jungilgyu <wjddlfrb131@naver.com>
2024-02-07 13:35:53 +0900

    dd

--- FrontEnd/src/components/Login/OAuth.jsx
+++ FrontEnd/src/components/Login/OAuth.jsx
@@ -4,3 +4,4 @@ const Redirect_URI = 'https://example.com/oauth'
 
 export const KAKAO_AUTH_URL = `http://localhost:8080/oauth2/authorization/kakao`
 // export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${Client_id}&redirect_uri=${Redirect_URI}&response_type=code`
+//dd
