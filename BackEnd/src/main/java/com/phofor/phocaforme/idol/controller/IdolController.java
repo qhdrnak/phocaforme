3bdf57b34710902dbe3946fae356189e960ded9d
MinY-Lee <leemy08@naver.com>
2024-02-09 16:53:41 +0900

    add: IdolController 추가

--- BackEnd/src/main/java/com/phofor/phocaforme/idol/controller/IdolController.java
+++ BackEnd/src/main/java/com/phofor/phocaforme/idol/controller/IdolController.java
@@ -1,42 +1,37 @@
-package com.phofor.phocaforme.auth.controller;
+package com.phofor.phocaforme.idol.controller;
 
 import com.phofor.phocaforme.idol.dto.response.IdolGroupResponseDto;
 import com.phofor.phocaforme.idol.dto.response.IdolMemberResponseDto;
-import com.phofor.phocaforme.idol.entity.IdolMember;
 import com.phofor.phocaforme.idol.service.IdolSelectService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Slf4j
-@RequestMapping("/user")
 @RequiredArgsConstructor
 @Controller
-public class BiasSelectController {
+public class IdolController {
     // 1. 들어오자마자 누르면 아이돌 그룹 뿌려주기
     // 2. 아이돌 그룹 선택하면 해당하는 멤버 부려주기
 
     private final IdolSelectService idolSelectService;
-//
-//    // 모든 아이돌 그룹 리스트 반환
-//    @GetMapping("/idol/group")
-//    public ResponseEntity<List<IdolGroupResponseDto>> getIdolGroupAll(){
-//        return ResponseEntity.ok().body(idolSelectService.findAll());
-//    }
-//
-//    @GetMapping("/idol/member/{idolGroupId}")
-//    public ResponseEntity<List<IdolMemberResponseDto>> getIdolMemberAll(@PathVariable Long idolGroupId) {
-//        return ResponseEntity.ok().body(idolSelectService.getAllByIdolGroupId(idolGroupId));
-//    }
 
-//    @PutMapping()
-//    public ResponseEntity<>
+
+    // 모든 아이돌 그룹 리스트 반환
+    @GetMapping("/idol/group")
+    public ResponseEntity<List<IdolGroupResponseDto>> getIdolGroupAll(){
+        return ResponseEntity.ok().body(idolSelectService.findAll());
+    }
+
+    @GetMapping("/idol/member/{idolGroupId}")
+    public ResponseEntity<List<IdolMemberResponseDto>> getIdolMemberAll(@PathVariable Long idolGroupId) {
+        return ResponseEntity.ok().body(idolSelectService.getAllByIdolGroupId(idolGroupId));
+    }
+
 }
