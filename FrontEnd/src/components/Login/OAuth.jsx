27ec95fe299ab3e292538c43131074bd5a75eb57
정민지 <mjje27@gmail.com>
2024-02-13 10:36:55 +0900

    FIX : React .env 설정

--- FrontEnd/src/components/Login/OAuth.jsx
+++ FrontEnd/src/components/Login/OAuth.jsx
@@ -4,5 +4,5 @@ const Redirect_URI = 'https://localhost:3000/main'
 
 export const KAKAO_AUTH_URL = process.env.REACT_APP_LOGIN_API_URL + `oauth2/authorization/kakao`;
 
-    `http://phocafor.me:4885/oauth2/authorization/kakao`
+// `http://phocafor.me:4885/oauth2/authorization/kakao`
 // export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${Client_id}&redirect_uri=${Redirect_URI}&response_type=code`
