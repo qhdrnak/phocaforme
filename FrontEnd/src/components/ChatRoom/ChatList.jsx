6f4ff0a42c5dc8683fc50eeb9a3ec09852889850
서울_8반_유혜승 <h103906@gmail.com>
2024-02-13 10:34:55 +0900

    feat: 안읽은 채팅 표시

--- FrontEnd/src/components/ChatRoom/ChatList.jsx
+++ FrontEnd/src/components/ChatRoom/ChatList.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { useSelector,  } from "react-redux";
-import { useNavigate,  } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import axios from 'axios';
+import axios from "axios";
 
 import { timeFormat } from "../../utils/timeFormat";
 
@@ -16,7 +16,7 @@ const ChatList = () => {
   };
 
   const loginUser = useSelector((state) =>
-    state.loginUser ? state.loginUser.user.userId : null
+    state.user ? state.user.user.userId : null
   );
 
   const [chatLists, setChatLists] = useState([]);
@@ -25,65 +25,86 @@ const ChatList = () => {
   useEffect(() => {
     const fetchData = async () => {
       await axios
-      .get(`http://localhost:8080/chatRoom`, 
-      {
-        withCredentials: true,
-      })
-      .then(response => {
-        setChatLists(response.data);
-      })
-      .catch(error => {
-        console.error('Error ChatList:', error);
-      });
-    }
+        .get(`http://localhost:8080/chatRoom`, {
+          withCredentials: true,
+        })
+        .then((response) => {
+          setChatLists(response.data);
+        })
+        .catch((error) => {
+          console.error("Error ChatList:", error);
+        });
+    };
     fetchData();
   }, []);
+
+  console.log(chatLists);
+  console.log(loginUser);
 
   return (
     <div>
       <h1 className="chat-title">채팅목록</h1>
-      <List sx={{ width: "100%", maxWidth: 500 }}>
-        {chatLists.map((chatroom, index) => (
-          <ListItem
-            className="chatlist-item"
-            key={index}
-            onClick={() =>
-              moveChatRoom(chatroom.chatRoomId, chatroom)
-            }
-          >
-            <ListItemText
-              primary={
-                chatroom.ownerId !== loginUser
-                  ? chatroom.visiterId
-                  : chatroom.ownerId
+      {chatLists.length == 0 ? (
+        <div className="chat-title">현재 진행중인 채팅이 없습니다!</div>
+      ) : (
+        <List sx={{ width: "100%", maxWidth: 500 }}>
+          {chatLists.map((chatroom, index) => (
+            <ListItem
+              className={
+                (chatroom.visiterId == loginUser &&
+                  chatroom.latestChat.id !== chatroom.visitorLatestChatId) ||
+                (chatroom.ownerId == loginUser &&
+                  chatroom.latestChat.id !== chatroom.ownerLatestChatId)
+                  ? "unread-chatlist-item"
+                  : "chatlist-item"
               }
-              secondary={
-                <div id='chatlist-info'>
-
-                <React.Fragment>
-                  <Typography
-                    // sx={{ display: "inline" }}
-                    component="div"
-                    variant="body2"
-                    color="text.primary"
-                  >
-                    {chatroom.latestChat ? chatroom.latestChat.message : "(이미지)"}
-                  </Typography>
-                  <Typography>
-                    {chatroom.latestChat ? `${timeFormat(chatroom.latestChat.createdAt)}` : null}
-                  </Typography>
-                </React.Fragment>
-                </div>
-              }
+              key={index}
+              onClick={() => moveChatRoom(chatroom.chatRoomId, chatroom)}
+            >
+              <ListItemText
+                primary={
+                  chatroom.ownerId !== loginUser
+                    ? chatroom.visiterId
+                    : chatroom.ownerId
+                }
+                secondary={
+                  <div id="chatlist-info">
+                    <React.Fragment>
+                      <Typography
+                        // sx={{ display: "inline" }}
+                        component="div"
+                        variant="body2"
+                        color="text.primary"
+                      >
+                        {chatroom.latestChat
+                          ? chatroom.latestChat.message
+                          : "(이미지)"}
+                      </Typography>
+                      <Typography>
+                        {chatroom.latestChat
+                          ? `${timeFormat(chatroom.latestChat.createdAt)}`
+                          : null}
+                      </Typography>
+                    </React.Fragment>
+                  </div>
+                }
               />
-            <Badge
-              badgeContent={chatroom.ownerLatestChatId !== chatroom.visitorLatestChatId ? "T" : "F"}
-              color="primary"
-              sx={{ marginRight: 2 }}
-            ></Badge>
-          </ListItem>
-        ))}
-      </List>
+              {/* <Badge
+                badgeContent={
+                  (chatroom.visiterId == loginUser &&
+                    chatroom.latestChat.id !== chatroom.visitorLatestChatId) ||
+                  (chatroom.ownerId == loginUser &&
+                    chatroom.latestChat.id !== chatroom.ownerLatestChatId)
+                    ? "chatlist-item"
+                    : "unread-chatlist-item"
+                }
+                color="primary"
+                sx={{ marginRight: 2 }}
+              ></Badge> */}
+            </ListItem>
+          ))}
+        </List>
+      )}
     </div>
   );
 };
