dfdbc974899d2e3250f8ef1ba63c8b360dc6867d
김봉균 <qhdrnak@hanyang.ac.kr>
2024-02-14 16:56:30 +0900

    fix: fix sql ddl

--- BackEnd/src/main/java/com/phofor/phocaforme/db/initdb/d/04barter.sql
+++ BackEnd/src/main/java/com/phofor/phocaforme/db/initdb/d/04barter.sql
@@ -6,10 +6,10 @@ CREATE TABLE barter
     barter_title     VARCHAR(255) NULL,
     barter_content   VARCHAR(255) NULL,
     barter_card_type VARCHAR(255) NULL,
-    barterStatus     TINYINT(1) DEFAULT 0,
+    barter_status     TINYINT(1) DEFAULT 0,
     bartered         TINYINT(1) DEFAULT 0,
     barter_group_id BIGINT NULL,
-    registration_date DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) NULL,
+    created_date DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) NULL,
     last_modified_date DATETIME(6) DEFAULT CURRENT_TIMESTAMP,
     user_id          CHAR(50) NULL,
     CONSTRAINT FK_Barter_User FOREIGN KEY (user_id) REFERENCES user (user_id)
