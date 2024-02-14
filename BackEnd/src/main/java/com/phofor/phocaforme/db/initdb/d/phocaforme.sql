0dd8463db863f175bd75453428a9138050f2ffd7
이민영 <leemy08@naver.com>
2024-02-14 12:55:22 +0900

    fix: phocaforme sql 수정

--- BackEnd/src/main/java/com/phofor/phocaforme/db/initdb/d/phocaforme.sql
+++ BackEnd/src/main/java/com/phofor/phocaforme/db/initdb/d/phocaforme.sql
@@ -196,6 +196,7 @@ VALUES (1, 'female', 'Girls\' Generation', '소녀시대'),
        (146, 'male', 'TIOT', '티아이오티');
 
 
+
 CREATE TABLE `idol_member`
 (
     `idol_group_id`  bigint(20)   DEFAULT NULL,
@@ -1051,3 +1052,39 @@ CREATE TABLE `barter_chat_room`
   COLLATE = utf8mb4_general_ci;
 
 
+-- user wish card table
+
+create table user_wish_card
+(
+    user_wish_id BIGINT primary key AUTO_INCREMENT not null ,
+    user_wish_user_id CHAR(50) unique not null,
+    foreign key (user_wish_user_id) references user (user_id),
+    user_wish_idol_member_id BIGINT(20) not null,
+    foreign key (user_wish_idol_member_id) references idol_member (idol_member_id),
+    user_wish_keyword1 VARCHAR(50) unique,
+    user_wish_keyword2 VARCHAR(50) unique,
+    user_wish_keyword3 VARCHAR(50) unique,
+    user_wish_created_at  TIMESTAMP not null default current_timestamp,
+    user_wish_updated_at  TIMESTAMP not null default current_timestamp
+);
+
+
+-- idol rank table
+
+CREATE TABLE idol_rank
+(
+                           idol_rank_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                           first_female_idol_id BIGINT,
+                           second_female_idol_id BIGINT,
+                           third_female_idol_id BIGINT,
+                           first_male_idol_id BIGINT,
+                           second_male_idol_id BIGINT,
+                           third_male_idol_id BIGINT,
+                           created_date DATE,
+                           CONSTRAINT FK_first_female_idol FOREIGN KEY (first_female_idol_id) REFERENCES idol_member(idol_member_id),
+                           CONSTRAINT FK_second_female_idol FOREIGN KEY (second_female_idol_id) REFERENCES idol_member(idol_member_id),
+                           CONSTRAINT FK_third_female_idol FOREIGN KEY (third_female_idol_id) REFERENCES idol_member(idol_member_id),
+                           CONSTRAINT FK_first_male_idol FOREIGN KEY (first_male_idol_id) REFERENCES idol_member(idol_member_id),
+                           CONSTRAINT FK_second_male_idol FOREIGN KEY (second_male_idol_id) REFERENCES idol_member(idol_member_id),
+                           CONSTRAINT FK_third_male_idol FOREIGN KEY (third_male_idol_id) REFERENCES idol_member(idol_member_id)
+);
