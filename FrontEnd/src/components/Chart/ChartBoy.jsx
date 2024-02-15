135b1301680bf90a6cb61cc832b51c44e5146daf
서울_8반_유혜승 <h103906@gmail.com>
2024-02-15 15:44:27 +0900

    fix: 랭킹 순위 텍스트 수정

--- FrontEnd/src/components/Chart/ChartBoy.jsx
+++ FrontEnd/src/components/Chart/ChartBoy.jsx
@@ -17,18 +17,18 @@ const ChartBoy = ({ rankBoy }) => {
       ) : (
         <Grid container spacing={2} direction="row" id="chart-container">
           <Grid item xs={6} id="rank-1-idol">
-            <div className="main-big">1. </div>
+            <div className="main-big">1위</div>
             <Avatar id="rank-1-image" src={rankBoy[0].idolImage} />
             <div className="main-big">{rankBoy[0].idolName}</div>
           </Grid>
           <Grid item id="rank-23-idol" xs={6}>
             <div id="rank-2-idol">
-              <div>2. </div>
+              <div>2위</div>
               <Avatar className="rank-23-image" src={rankBoy[1].idolImage} />
               <div>{rankBoy[1].idolName}</div>
             </div>
             <div id="rank-3-idol">
-              <div>3. </div>
+              <div>3위</div>
               <Avatar className="rank-23-image" src={rankBoy[2].idolImage} />
               <div>{rankBoy[2].idolName}</div>
             </div>
--- FrontEnd/src/components/Chart/ChartGirl.jsx
+++ FrontEnd/src/components/Chart/ChartGirl.jsx
@@ -17,18 +17,18 @@ const ChartGirl = ({ rankGirl }) => {
       ) : (
         <Grid container spacing={2} direction="row" id="chart-container">
           <Grid item xs={6} id="rank-1-idol">
-            <div className="main-big">1. </div>
+            <div className="main-big">1위</div>
             <Avatar id="rank-1-image" src={rankGirl[0].idolImage} />
             <div className="main-big">{rankGirl[0].idolName}</div>
           </Grid>
           <Grid item id="rank-23-idol" xs={6}>
             <div id="rank-2-idol">
-              <div>2. </div>
+              <div>2위</div>
               <Avatar className="rank-23-image" src={rankGirl[1].idolImage} />
               <div>{rankGirl[1].idolName}</div>
             </div>
             <div id="rank-3-idol">
-              <div>3. </div>
+              <div>3위</div>
               <Avatar className="rank-23-image" src={rankGirl[2].idolImage} />
               <div>{rankGirl[2].idolName}</div>
             </div>
