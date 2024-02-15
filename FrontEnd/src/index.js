8bf6f669df2e51d7b7f489bf73b4be592c28a092
Jungilgyu <wjddlfrb131@naver.com>
2024-02-15 16:24:40 +0900

    feat: Modify

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -7,7 +7,7 @@ import { CookiesProvider } from 'react-cookie';
 // 서비스 워커 등록
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./public/firebase-messaging-sw.js')
+    navigator.serviceWorker.register('/firebase-messaging-sw.js')
       .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
