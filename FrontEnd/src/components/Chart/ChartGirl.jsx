291996e9c8722bc46b4161e4ffe41f59000d4a5e
HYESEUNG <h103906@naver.com>
2024-02-16 00:40:17 +0900

    design: 포포차트 디자인 수정

--- FrontEnd/src/components/Chart/ChartGirl.jsx
+++ FrontEnd/src/components/Chart/ChartGirl.jsx
@@ -18,12 +18,12 @@ const ChartGirl = ({ isNull, rankGirl }) => {
         <CircularProgress />
       ) : (
         <Grid container spacing={2} direction="row" id="chart-container">
-          <Grid item xs={6} id="rank-1-idol">
+          <Grid item xs={7} id="rank-1-idol">
             <div className="main-big">1위</div>
             <Avatar id="rank-1-image" src={rankGirl[0].idolImage} />
             <div className="main-big">{rankGirl[0].idolName}</div>
           </Grid>
-          <Grid item id="rank-23-idol" xs={6}>
+          <Grid item id="rank-23-idol" xs={5}>
             <div id="rank-2-idol">
               <div>2위</div>
               <Avatar className="rank-23-image" src={rankGirl[1].idolImage} />
