949c3b3b43c1521a9fb3900bb59ddef0c45f5f84
김강민 <mmm3657@naver.com>
2024-02-16 11:23:43 +0900

    refactor: 갈망포카 DB 수정

--- BackEnd/src/main/java/com/phofor/phocaforme/db/initdb/d/12user_wish_card.sql
+++ BackEnd/src/main/java/com/phofor/phocaforme/db/initdb/d/12user_wish_card.sql
@@ -5,9 +5,9 @@ create table user_wish_card
     foreign key (user_wish_user_id) references user (user_id),
     user_wish_idol_member_id BIGINT(20) not null,
     foreign key (user_wish_idol_member_id) references idol_member (idol_member_id),
-    user_wish_keyword1 VARCHAR(50) unique,
-    user_wish_keyword2 VARCHAR(50) unique,
-    user_wish_keyword3 VARCHAR(50) unique,
+    user_wish_keyword1 VARCHAR(50),
+    user_wish_keyword2 VARCHAR(50),
+    user_wish_keyword3 VARCHAR(50),
     user_wish_created_at  TIMESTAMP not null default current_timestamp,
     user_wish_updated_at  TIMESTAMP not null default current_timestamp
 );
